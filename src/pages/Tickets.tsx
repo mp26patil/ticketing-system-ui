93d8f52aecb4b4c8eb84137ebec1f00dda257f1f
Prakash Patil <prakashpatil.cp@gmail.com>
2026-02-10 08:07:19 +0530

    table format is added to ticketing page

--- src/pages/Tickets.tsx
+++ src/pages/Tickets.tsx
@@ -1,7 +1,24 @@
 const Tickets = () => {
   return (
     <div>   
-      <h2>Tickets</h2>
+      <tables>
+  <tr>
+    <th>Ticket ID</th>
+    <th>Subject</th>
+    <th>Status</th>
+  </tr>
+  <tr>
+    <td>101</td>
+    <td>Cannot sign in</td>
+    <td>Open</td>
+  </tr>
+  <tr>
+    <td>102</td>
+    <td>Error on checkout</td>
+    <td>In Progress</td>
+  </tr>
+</table>
+
     </div>
   );
 }
